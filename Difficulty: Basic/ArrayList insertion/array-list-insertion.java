2d09217e99504a269fb899d1dfc7dc0bb5a9e401
PENDYALA PUJITHA DEVI <23mh1a4946@acoe.edu.in>
2026-07-24 11:37:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/ArrayList insertion/array-list-insertion.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static ArrayList<Integer> fillArrayList(int arr[]) {
+        // Your code here
+        ArrayList<Integer> list = new ArrayList<>();
+
+for(int i = 0; i < arr.length; i++) {
+    list.add(arr[i]);
+}
+
+return list;
+    }
+}
